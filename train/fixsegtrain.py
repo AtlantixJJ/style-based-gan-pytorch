b2724810257c44c5aa5218df6623cfa5cb86713a
jianjin xu <jianjinxu@dyn-160-39-150-86.dyn.columbia.edu>
2019-11-06 13:57:39 -0500

    git sync

--- train/fixsegtrain.py
+++ train/fixsegtrain.py
@@ -1,3 +1,6 @@
+"""
+Teacher student training of SRGAN.
+"""
 import sys
 sys.path.insert(0, ".")
 import os
@@ -11,77 +14,78 @@ from lib.face_parsing import unet
 import config
 from utils import *
 from loss import *
-from model.seg import StyledGenerator
+import model
 
-STEP = 8 # 1024px resolution
+STEP = 8
 ALPHA = 1
 
 cfg = config.SConfig()
 cfg.parse()
 cfg.print_info()
 cfg.setup()
-# GPU1: main network + auxiliary network GPU2: extractor
+
 state_dict = torch.load("checkpoint/faceparse_unet.pth", map_location='cpu')
 faceparser = unet.unet()
 faceparser.load_state_dict(state_dict)
-faceparser = faceparser.to(cfg.device)
+faceparser = faceparser.cuda()
 faceparser.eval()
 del state_dict
 
-state_dicts = torch.load(cfg.load_path, map_location='cpu')
-sg = StyledGenerator(512, semantic=cfg.semantic_config)
-sg.load_state_dict(state_dicts['generator'])
+sg = model.tfseg.StyledGenerator(semantic=cfg.semantic_config)
+sg.load_state_dict(state_dicts, strict=False)
 sg.train()
-sg = sg.to(cfg.device)
-# the extractor module in on another GPU
-#for i, blk in enumerate(sg.generator.progression):
-#	if blk.n_class > 0:
-#		blk.extractor = blk.extractor.to(cfg.device2)
+sg = sg.cuda()
+sg.freeze_g_mapping() # do not change style branch
+sg.freeze_g_synthesis()
 del state_dicts
 
 # new parameter adaption stage
-g_optim = torch.optim.Adam(get_generator_extractor_lr(
-    sg.generator, cfg.lr), betas=(0.9, 0.9))
+g_optim = torch.optim.Adam(sg.semantic_branch.parameters(),
+	lr=cfg.lr,
+	betas=(0.9, 0.9)) # 1e-3
+g_optim.add_param_group({
+    'params': sg.g_synthesis.parameters(),
+    'lr': cfg.lr * 0.1})
 logsoftmax = torch.nn.CrossEntropyLoss()
-logsoftmax = logsoftmax.to(cfg.device2)
+logsoftmax = logsoftmax.cuda()
 
-latent = torch.randn(cfg.batch_size, 512).to(cfg.device)
+latent = torch.randn(cfg.batch_size, 512).cuda()
 noise = [0] * (STEP + 1)
 for k in range(STEP + 1):
     size = 4 * 2 ** k
-    noise[k] = torch.randn(cfg.batch_size, 1, size, size).to(cfg.device)
+    noise[k] = torch.randn(cfg.batch_size, 1, size, size).cuda()
 
-record = {"loss":[], "segloss":[]}
+record = cfg.record
+avgmseloss = 0
 count = 0
 
-for i in tqdm(range(cfg.n_iter + 1)):
+for i in tqdm(range(cfg.n_iter)):
+	if i == 1000:
+		sg.freeze_g_synthesis(train=True)
+
 	latent.normal_()
 	for k in range(STEP + 1):
 		noise[k].normal_()
 
+	gen = sg(latent)
 	with torch.no_grad():
-		gen = sg(latent,
-			noise=noise,
-			step=STEP,
-			alpha=ALPHA)
 		gen = F.interpolate(gen, cfg.imsize, mode="bilinear")
-		label = faceparser(gen).argmax(1).detach()
+		label = faceparser(gen).argmax(1)
 
-	segs = []
-	for j in range(STEP + 1):
-		if sg.generator.progression[j].n_class <= 0:
-			continue
-		segmentation = get_segmentation(sg.generator.progression, j)[0]
-		segs.append(segmentation)
-		segloss = logsoftmax(F.interpolate(
-			segmentation,
-			label.shape[2:],
-			mode="bilinear"), label)
-		loss = segloss
-		with torch.autograd.detect_anomaly():
-			loss.backward()
-		g_optim.step()
-		g_optim.zero_grad()
+	segs = sg.extract_segmentation()
+	seglosses = []
+	for s in segs:
+		seglosses.append(logsoftmax(
+			F.interpolate(s, label.shape[2:], mode="bilinear"),
+			label))
+	segloss = cfg.seg_coef * sum(seglosses) / len(seglosses)
+
+	loss = segloss
+	with torch.autograd.detect_anomaly():
+		loss.backward()
+
+	g_optim.step()
+	g_optim.zero_grad()
 
 	record['loss'].append(torch2numpy(loss))
 	record['segloss'].append(torch2numpy(segloss))
@@ -93,18 +97,21 @@ for i in tqdm(range(cfg.n_iter + 1)):
 			l.append(record[k][-1])
 		print(l)
 
-	if i % 100 == 0 and i > 0:
+	if i % 1000 == 0 and i > 0:
 		print("=> Snapshot model %d" % i)
 		torch.save(sg.state_dict(), cfg.expr_dir + "/iter_%06d.model" % i)
 
-	if i % 100 == 0 or cfg.debug:
+	if i % 1000 == 0 or cfg.debug:
+		vutils.save_image(gen[:4], cfg.expr_dir + '/gen_%06d.png' % i,
+							nrow=2, normalize=True, range=(-1, 1))
+							
 		tarlabels = [torch.from_numpy(tensor2label(
 						label[i:i+1],
 						label.shape[1]))
 						for i in range(label.shape[0])]
 		tarlabels = [l.float().unsqueeze(0) for l in tarlabels]
 		tarviz = torch.cat([F.interpolate(m, 256).cpu() for m in tarlabels])
-		genlabels = [torch.from_numpy(tensor2label(s[0], s.shape[1]))
+		genlabels = [torch.from_numpy(tensor2label(s[0].argmax(0), s.shape[1]))
 					for s in segs]
 		genlabels = [l.float().unsqueeze(0) for l in genlabels]
 		genviz = genlabels + [(gen[0:1] + 1) / 2]
@@ -113,5 +120,6 @@ for i in tqdm(range(cfg.n_iter + 1)):
 		vutils.save_image(tarviz, cfg.expr_dir + "/tarlabel_viz_%05d.png" % i, nrow=2)
 
 		write_log(cfg.expr_dir, record)
+		plot_dic(record, cfg.expr_dir + "/loss.png")
 
 os.system("python script/monitor.py --task log,seg --model %s --step 8" % cfg.expr_dir)
