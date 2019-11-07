34dc52ae9925f69225fd82bada88db07d9ddd70b
jianjin xu <jianjinxu@dyn-160-39-150-86.dyn.columbia.edu>
2019-11-07 16:31:03 -0500

    git sync

--- script/trainall.py
+++ script/trainall.py
@@ -3,10 +3,10 @@ import os
 class TSSeg(object):
     def __init__(self):
         self.seg_cfgs = [
-            "3res1-64-19",
-            "3res2-64-19",
-            "3conv1-64-19",
-            "3conv2-64-19"
+            "3res1-64-16",
+            "3res2-64-16",
+            "3conv1-64-16",
+            "3conv2-64-16"
             ]
         self.basecmd = "python train/fixsegtrain.py --task fixseg --seg-cfg %s --gpu %s --batch_size %d --iter-num 1000 &"
     
