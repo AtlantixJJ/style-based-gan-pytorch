3e4b7290d7c2b6075b1ee013fb6e90f3bb625815
jianjin xu <jianjinxu@dyn-160-39-150-86.dyn.columbia.edu>
2019-11-07 17:07:23 -0500

    git sync

--- test_unet.py
+++ test_unet.py
@@ -26,7 +26,7 @@ del state_dict
 
 evaluator = utils.MaskCelebAEval(map_id=True)
 tar_record = {i:[] for i in range(0, evaluator.n_class)}
-for i, latent, image, label in enumerate(ds):
+for i, (latent, image, label) in enumerate(ds):
     print(i)
     image = torch.from_numpy(image).float().cuda()
     image = (image.permute(2, 0, 1) - 127.5) / 127.5
