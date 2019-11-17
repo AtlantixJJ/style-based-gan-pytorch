173592ec703c82cfa05e2aee3f56c0f00e3a63f1
jianjin xu <jianjinxu@jianjindembp.home>
2019-11-16 23:14:22 -0500

    git sync

--- test.py
+++ test.py
@@ -83,8 +83,8 @@ for i, (latent_np, image_np, label_np) in enumerate(tqdm(ds)):
     if i < 4:
         image = torch.from_numpy(image_np).float()
         image = image.permute(2, 0, 1).unsqueeze(0) / 255.
-        genlabel = utils.tensor2label(torch.frum_numpy(seg), ds.n_class)
-        tarlabel = utils.tensor2label(torch.frum_numpy(label), ds.n_class)
+        genlabel = utils.tensor2label(torch.from_numpy(seg), ds.n_class)
+        tarlabel = utils.tensor2label(torch.from_numpy(label), ds.n_class)
         gen = gen.unsqueeze(0)
         res = [image, genlabel, gen, tarlabel]
         res = torch.cat([F.interpolate(item, (256, 256), mode="bilinear") for item in res])
