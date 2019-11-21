3384574847b3794413c41201349423faaa729f02
jianjin xu <jianjinxu@dyn-160-39-150-171.dyn.columbia.edu>
2019-11-20 20:03:04 -0500

    git sync

--- test_unet.py
+++ test_unet.py
@@ -17,6 +17,14 @@ ds = dataset.LatentSegmentationDataset(
     image_dir=rootdir+"CelebA-HQ-img",
     seg_dir=rootdir+"CelebAMask-HQ-mask")
 
+generator = StyledGenerator()
+state_dict = torch.load("checkpoint/karras2019stylegan-ffhq-1024x1024.for_g_all.pt", map_location='cpu')
+generator.load_state_dict(state_dict)
+generator = generator.cuda()
+generator.eval()
+del state_dict
+
+
 state_dict = torch.load("checkpoint/faceparse_unet.pth", map_location='cpu')
 faceparser = unet.unet()
 faceparser.load_state_dict(state_dict)
@@ -27,9 +35,11 @@ del state_dict
 evaluator = utils.MaskCelebAEval(map_id=True)
 tar_record = {i:[] for i in range(0, evaluator.n_class)}
 for i, (latent, image, label) in enumerate(ds):
-    image = torch.from_numpy(image).float().cuda()
-    image = (image.permute(2, 0, 1) - 127.5) / 127.5
+    #image = torch.from_numpy(image).float().cuda()
+    #image = (image.permute(2, 0, 1) - 127.5) / 127.5
+    latent = torch.from_numpy(latent_np).unsqueeze(0).float().cuda()
     with torch.no_grad():
+        image = generator(latent).clamp(-1, 1)
         image_ = F.interpolate(image.unsqueeze(0), (512, 512))
         tar_seg = faceparser(image_)[0]
         tar_seg = tar_seg.argmax(0).detach().cpu().numpy()
