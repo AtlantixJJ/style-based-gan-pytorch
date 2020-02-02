03c094af0924cb34bf44cba872ed36fd17e6a1b4
Jianjin Xu <jx2386@columbia.edu>
2020-02-02 17:04:19 -0500

    git sync

--- optim.py
+++ optim.py
@@ -91,7 +91,7 @@ def extended_latent_edit_label_stroke(model, latent, noises, label_stroke, label
         celoss = mask_cross_entropy_loss(diff_mask, seg, target_label)
         loss = mseloss + celoss
         grad = torch.autograd.grad(loss, latent)[0]
-        print(grad.shape, grad.min(), grad.max())
+        print("%.3f\t%.3f\t%.3f\t%.3f" % (celoss, gradnorm, grad.min(), grad.max()))
         grad_norm = torch.norm(grad.view(-1), 2)
         for i in range(len(latents)):
             latents[i].grad = grad[:, i:i+1]
