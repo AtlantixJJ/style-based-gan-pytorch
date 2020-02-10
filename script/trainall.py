2ba4b6114718912aeaf75023e7f4c9c9640a22fd
Jianjin Xu <jx2386@columbia.edu>
2020-02-10 15:16:05 -0500

    git sync

--- script/trainall.py
+++ script/trainall.py
@@ -12,13 +12,15 @@ class FixSeg(object):
             1e-3,
             5e-3,
         ]
-        self.basecmd = "python train/fixsegtrain.py --task fixseg --seg-cfg %s --gpu %s --batch_size %d --iter-num %d --trace %d --load checkpoint/karras2019stylegan-celebahq-1024x1024.for_g_all.pt &"
+        self.basecmd = "python train/fixsegtrain.py --task fixseg --seg-cfg %s --gpu %s --batch_size %d --iter-num %d --trace %d --reg-coef %f --load checkpoint/karras2019stylegan-celebahq-1024x1024.for_g_all.pt &"
     
     def args_gen(self, gpus):
         l = []
         count = 0
  
-        for segcfg in self.seg_cfgs:
+        for i in range(len(self.seg_cfgs)):
+            segcfg = self.seg_cfgs[i]
+            l1coef = self.l1_coefs[i]
             gpu = gpus[count]
 
             trace = batch_size = iter_num = 0
@@ -31,7 +33,7 @@ class FixSeg(object):
                 batch_size = 4
                 iter_num = 8000
 
-            l.append((count, (segcfg, gpu, batch_size, iter_num, trace)))
+            l.append((count, (segcfg, gpu, batch_size, iter_num, trace, l1coef)))
             count = (count + 1) % len(gpus)
         return l
     
