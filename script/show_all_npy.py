6c9fee1a7b37ba005bc6647cea88a192f3261083
Jianjin Xu <jx2386@columbia.edu>
2020-02-17 16:30:35 -0500

    After meeting

--- script/show_all_npy.py
+++ script/show_all_npy.py
@@ -4,8 +4,14 @@ import numpy as np
 import glob
 import utils
 
+# plot fid data
+dic = {}
+with open("/Users/jianjinxu/temp/var_metric/wgan128/wgan128_fid.txt", "r") as f:
+    dic["wgan128"] = [float(num.strip()) for num in f.readlines()]
+utils.plot_dic(dic, "FID", "wgan128_FID.png")
+
 # analyze classwise data
-files = glob.glob("results/var_metric/*class*.npy")
+files = glob.glob("/Users/jianjinxu/temp/var_metric/wgan128/*class_dic.npy")
 dic = np.load(files[0], allow_pickle=True)[()]
 summary = {k:[] for k in dic.keys()}
 
@@ -13,9 +19,15 @@ for f in files:
     dic = np.load(f, allow_pickle=True)[()]
     for k,v in dic.items():
         v = np.array(v)
-        mean = v.mean()
-        std = v.std()
-        if mean < 0:
-            std = -1
-        summary[k].append(std)
+        mean = np.abs(v[1:] - v[:-1]).mean()
+        #std = v[start_iteration:].std()
+        summary[k].append(mean)
+# filter out outlier
+for k in summary.keys():
+    arr = np.array(summary[k])
+    mu = arr.mean()
+    std = arr.std()
+    arr[(arr - mu) > std] = mu + std
+    arr[(mu - arr) > std] = mu - std
+    summary[k] = arr
 utils.plot_dic(summary, "class linearity", "class.png")
