8ed225d07b732f215b60adb969fdc5182ff5678a
Jianjin Xu <jx2386@columbia.edu>
2020-02-19 11:26:06 -0500

    Add compile

---
+++ script/compile_fewshot.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.insert(0, ".")
+import copy
+import numpy as np
+import glob
+import utils
+
+
+# analyze global data
+files = glob.glob("results/*class.npy")
+result = {}
+
+def get_name(name):
+    ind = name.rfind("/")
+    s = "_".join(name[ind + 1:].split("_")[1:4])
+    #if "global" in name:
+    #    s += "_global"
+    #else:
+    #    s += "_class"
+    return s
+
+def parse_key(key):
+    repeat, iteration, trainsize = key.split("_")
+    return int(iteration[1:]), int(trainsize[1:])
+
+
+for f in files:
+    name = get_name(f)
+    dic = np.load(f, allow_pickle=True)[()]
+    arr = np.array(dic["IoU"])
+    arr[0] = arr[13] = -1 # doesn't count background & necklace
+    arr = arr[arr > -0.1]
+    result[name] = arr.mean()
+
+ib_dic = {i:{b:[] for b in [1, 2, 3]} for i in [50, 100, 150, 200]}
+mean_dic = copy.deepcopy(ib_dic)
+std_dic = copy.deepcopy(ib_dic)
+for key in result.keys():
+    i, b = parse_key(key)
+    ib_dic[i][b].append(result[key])
+for i in ib_dic.keys():
+    for b in ib_dic[i].keys():
+        ib_dic[i][b] = np.array(ib_dic[i][b])
+for i in ib_dic.keys():
+    for b in ib_dic[i].keys():
+        mean_dic[i][b] = ib_dic[i][b].mean()
+        std_dic[i][b] = ib_dic[i][b].std()
+print(mean_dic)
+print(std_dic)
--- script/trainall_linear.py
+++ script/trainall_linear.py
@@ -1,6 +1,6 @@
 import os
 
-train_size = list(range(1, 12)); gpus = [0]
+train_size = list(range(3, 12)); gpus = [0]
 #train_size = list(range(12, 101, 4)); gpus = [4,5,6,7]
 
 basecmd = "python script/linear_multiple_image.py --train-size %d --gpu %d"
