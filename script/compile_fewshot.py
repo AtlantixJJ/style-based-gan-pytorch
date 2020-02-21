cb83af792d69de6851f579ff363eb7def17af981
Jianjin Xu <jx2386@columbia.edu>
2020-02-20 20:01:21 -0500

    few shot vis

--- script/compile_fewshot.py
+++ script/compile_fewshot.py
@@ -22,49 +22,53 @@ result = {}
 
 def get_name(name):
     ind = name.rfind("/")
-    s = "_".join(name[ind + 1:].split("_")[1:4])
+    s = "_".join(name[ind + 1:].split("_")[1:3])
     return s
 
 def parse_key(key):
-    repeat, iteration, trainsize = key.split("_")
-    return int(iteration[1:]), int(trainsize[1:])
+    repeat, trainsize = key.split("_")
+    return int(trainsize[1:])
 
 
 bs = []
 for f in files:
     name = get_name(f)
-    bs.append(int(name.split("_")[2][1:]))
+    bs.append(int(name.split("_")[1][1:]))
     dic = np.load(f, allow_pickle=True)[()]
     arr = np.array(dic["IoU"])
     arr[0] = arr[13] = -1 # doesn't count background & necklace
     arr = arr[arr > -0.1]
     result[name] = arr.mean()
 bs = np.unique(bs)
-ib_dic = {i:{b:[] for b in bs} for i in [50, 100, 150, 200]}
-mean_dic = copy.deepcopy(ib_dic)
-std_dic = copy.deepcopy(ib_dic)
+b_dic = {b:[] for b in bs}
+mean_dic = copy.deepcopy(b_dic)
+min_dic = copy.deepcopy(b_dic)
+max_dic = copy.deepcopy(b_dic)
 for key in result.keys():
-    i, b = parse_key(key)
-    ib_dic[i][b].append(result[key])
-for i in ib_dic.keys():
-    for b in ib_dic[i].keys():
-        ib_dic[i][b] = np.array(ib_dic[i][b])
-for i in ib_dic.keys():
-    for b in ib_dic[i].keys():
-        mean_dic[i][b] = ib_dic[i][b].mean()
-        std_dic[i][b] = ib_dic[i][b].std()
-print(mean_dic)
-print(std_dic)
+    b = parse_key(key)
+    b_dic[b].append(result[key])
+for b in b_dic.keys():
+    b_dic[b] = np.array(b_dic[b])
+for b in b_dic.keys():
+    mean_dic[b] = b_dic[b].mean()
+    min_dic[b] = b_dic[b].min()
+    max_dic[b] = b_dic[b].max()
 
 xs = []
 ys = []
 cs = []
-for i, iter in enumerate([50, 100, 150, 200]):
-    for b in mean_dic[iter].keys():
-        xs.append(b)
-        ys.append(mean_dic[iter][b])
-        cs.append(colors[i])
+for b in mean_dic.keys():
+    xs.append(b)
+    ys.append(mean_dic[b])
+    cs.append(colors[2])
+for b in mean_dic.keys():
+    xs.append(b)
+    ys.append(min_dic[b])
+    cs.append(colors[0])
+for b in mean_dic.keys():
+    xs.append(b)
+    ys.append(max_dic[b])
+    cs.append(colors[1])
 plt.scatter(xs, ys, c=cs)
 plt.savefig("scatter.png")
-plt.legend([50, 100, 150, 200])
 plt.close()
