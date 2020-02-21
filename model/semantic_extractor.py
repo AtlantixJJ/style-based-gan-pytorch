793269abb1e341ed87cebc099d291c938b7bf8d9
Jianjin Xu <jx2386@columbia.edu>
2020-02-21 21:52:00 +0000

    git sync

--- model/semantic_extractor.py
+++ model/semantic_extractor.py
@@ -129,8 +129,9 @@ class LinearSemanticExtractor(BaseSemanticExtractor):
         
     def forward_single_group(self, stage, last_only=False):
         outputs = []
-        print(self.semantic_extractor)
         for i, seg_input in enumerate(stage):
+            print(i, seg_input.shape)
+            print(self.semantic_extractor[i])
             outputs.append(self.semantic_extractor[i](seg_input))
         size = outputs[-1].shape[2]
 
