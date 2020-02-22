dbff27101f3b6ec3c7e1cee8a85435aeaf82313f
Jianjin Xu <jx2386@columbia.edu>
2020-02-22 01:30:55 +0000

    compile var

--- script/compile_var.py
+++ script/compile_var.py
@@ -12,36 +12,40 @@ style.use('ggplot')
 colors = list(matplotlib.colors.cnames.keys())
 
 data_dir = sys.argv[1]
-
+fid_dir = sys.argv[2]
+ind = fid_dir.rfind("/")
+fid_name = fid_dir[ind+1:-3]
 # plot fid data
 fids = {}
-with open("/Users/jianjinxu/temp/var_metric/wgan128/wgan128_fid.txt", "r") as f:
-    fids["wgan128"] = [float(num.strip()) for num in f.readlines()]
-utils.plot_dic(fids, "FID", "wgan128_FID.png")
+with open(fid_dir, "r") as f:
+    fids[fid_name] = [float(num.strip()) for num in f.readlines()]
+utils.plot_dic(fids, "FID", f"{fid_name}.png")
 
 # analyze global data
-files = glob.glob("/Users/jianjinxu/temp/var_metric/wgan128/*global_dic.npy")
+files = glob.glob(f"{data_dir}/*global_dic.npy")
 files.sort()
 dic = np.load(files[0], allow_pickle=True)[()]
 summary = {k:[] for k in dic.keys()}
-
 for f in files:
     dic = np.load(f, allow_pickle=True)[()]
     for k,v in dic.items():
+        if k == "pixelacc":
+            continue
+        print(k, v)
         v = np.array(v)
         x = np.abs(v[1:]-v[:-1]).mean()
         summary[k].append(x)
 
 utils.plot_dic(summary, "global linearity", "global.png")
 
-y = np.log(fids["wgan128"])
+y = np.log(fids[fid_name])
 x = summary["mIoU"][1:1+len(y)]
 plt.scatter(x, y)
 plt.savefig("correlation.png")
 plt.close()
 
 # analyze classwise data
-files = glob.glob("/Users/jianjinxu/temp/var_metric/wgan128/*class_dic.npy")
+files = glob.glob(f"{data_dir}/*class_dic.npy")
 files.sort()
 dic = np.load(files[0], allow_pickle=True)[()]
 summary = {k:[] for k in dic.keys()}
