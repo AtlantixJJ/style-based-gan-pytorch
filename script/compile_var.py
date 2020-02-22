f4c2678dd64e7ce6fb213ab51cdbcf3cbe591085
Jianjin Xu <jx2386@columbia.edu>
2020-02-22 18:40:32 -0500

    update compile

--- script/compile_var.py
+++ script/compile_var.py
@@ -15,7 +15,7 @@ data_dir = sys.argv[1]
 task = sys.argv[2]
 
 # analyze global data
-files = glob.glob(f"{data_dir}/*global_dic.npy")
+files = glob.glob(f"{data_dir}/{task}*global_dic.npy")
 files.sort()
 dic = np.load(files[0], allow_pickle=True)[()]
 summary = {k:[] for k in dic.keys()}
@@ -25,20 +25,20 @@ for f in files:
         v = np.array(v)
         x = np.abs(v[1:]-v[:-1]).mean()
         summary[k].append(x)
+utils.plot_dic(summary, "global linearity", "global.png")
 
 # analyze classwise data
-files = glob.glob(f"{data_dir}/*class_dic.npy")
+files = glob.glob(f"{data_dir}/{task}*class_dic.npy")
 files.sort()
 dic = np.load(files[0], allow_pickle=True)[()]
-summary = {k:[] for k in dic.keys()}
+summary = {k:[] for k in utils.CELEBA_REDUCED_CATEGORY}
 
 for f in files:
     dic = np.load(f, allow_pickle=True)[()]
-    for k,v in dic.items():
+    for i, v in enumerate(dic['IoU']):
         v = np.array(v)
         x = np.abs(v[1:]-v[:-1]).mean()
-        #std = v[start_iteration:].std()
-        summary[k].append(x)
+        summary[utils.CELEBA_REDUCED_CATEGORY[i]].append(x)
 
 utils.plot_dic(summary, "class linearity", "class.png")
 
