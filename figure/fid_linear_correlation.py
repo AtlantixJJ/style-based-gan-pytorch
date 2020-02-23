cd279a7d17f28d08dcf9e4cbab80bfa48c6a27f3
Jianjin Xu <jx2386@columbia.edu>
2020-02-23 12:32:28 -0500

    git sync

--- figure/fid_linear_correlation.py
+++ figure/fid_linear_correlation.py
@@ -44,6 +44,7 @@ for i, metric_name in enumerate(summary.keys()):
     y = y[1:minimum]
     x = x[1:minimum]
     ax = plt.subplot(1, 4, i + 1)
+    ax.set_title(metric_name)
     ax.scatter(x, y)
 plt.savefig(f"{task}_fid_linear_separability_correlation.png")
 plt.close()
