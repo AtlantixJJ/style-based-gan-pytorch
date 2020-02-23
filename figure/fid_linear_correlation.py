666ee7f41c2277a294bc861491274d88e43c40cf
Jianjin Xu <jx2386@columbia.edu>
2020-02-23 12:28:50 -0500

    git sync

--- figure/fid_linear_correlation.py
+++ figure/fid_linear_correlation.py
@@ -11,9 +11,10 @@ style.use('seaborn-poster') #sets the size of the charts
 style.use('ggplot')
 colors = list(matplotlib.colors.cnames.keys())
 
+imsize = sys.argv[1]
 data_dir = "results/"
-fid_dir = "record/wgan64/wgan64_fid.txt"
-task = sys.argv[1] #"celeba_wgan64"
+fid_dir = f"record/wgan{imsize}/wgan{imsize}_fid.txt"
+task = "celeba_wgan64" if imsize == "64" else "wgan128"
 ind = fid_dir.rfind("/")
 fid_name = fid_dir[ind+1:-3]
 
