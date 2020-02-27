12b6ee30ad6d29241d75e493e8e13f8f5b567f84
Jianjin Xu <jx2386@columbia.edu>
2020-02-27 11:28:48 -0500

    git sync

--- figure/imagelevel_cosine_similarity.py
+++ figure/imagelevel_cosine_similarity.py
@@ -36,7 +36,7 @@ std_table = (std_tables * size_tables).sum(0)
 cats = utils.CELEBA_REDUCED_CATEGORY
 # remove necklace because it is 0 everywhere
 del cats[13]
-del [0]
+del cats[0]
 
 def remove_numpy(x, idx):
     x = np.concatenate([x[:idx], x[idx+1:]], 0)
