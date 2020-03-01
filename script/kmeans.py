2ea54b59d656381273494163f6ac6fc10572144a
Jianjin Xu <jx2386@columbia.edu>
2020-03-01 03:48:33 +0000

    git sync

--- script/kmeans.py
+++ script/kmeans.py
@@ -5,22 +5,29 @@ from spherecluster import SphericalKMeans
 import pickle
 import argparse
 import utils
+import glob
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--load", default="feats.npy")
+parser.add_argument("--dataset", default="datasets/kmeans")
 args = parser.parse_args()
 
-feats = np.load(args.load, allow_pickle=True)
+files = glob.glob(f"{args.dataset}/kmeans_feats*.npy")
+files.sort()
+for f in files:
+    feats = np.load(f, allow_pickle=True)
+    ind = f.find("feats")
+    ind = int(f[ind+6:ind+7])
+    print(f, ind)
 
-H, W, C = feats.shape
-X = feats.reshape(H * W, C)#.reshape(C, H * W).transpose(1, 0)
+    H, W, C = feats.shape
+    X = feats.reshape(H * W, C)#.reshape(C, H * W).transpose(1, 0)
 
-for K in range(2, 16):
-    skm = SphericalKMeans(n_clusters=K, copy_x=False, n_jobs=40, verbose=True, max_iter=1000)
-    skm.fit(X)
-    pickle.dump(skm, open(f"{args.load}_skm_{K}.pkl", 'wb'))
+    for K in range(2, 16):
+        skm = SphericalKMeans(n_clusters=K, copy_x=True, n_jobs=4, verbose=True, max_iter=1000)
+        skm.fit(X)
+        pickle.dump(skm, open(f"{args.dataset}/skm_{ind}_{K}.pkl", 'wb'))
 
-    labels = skm.labels_.reshape(H, W)
-    label_viz = utils.numpy2label(labels, labels.max() + 1)
-    utils.imwrite(f"{args.load}_skm_{K}.png", label_viz)
+        labels = skm.labels_.reshape(H, W)
+        label_viz = utils.numpy2label(labels, labels.max() + 1)
+        utils.imwrite(f"{args.dataset}/skm_{ind}_{K}.png", label_viz)
