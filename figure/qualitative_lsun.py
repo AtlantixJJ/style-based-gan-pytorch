c14a37330863e09d042fd698a0dfc63f83c341b2
Jianjin Xu <jx2386@columbia.edu>
2020-03-01 15:45:36 -0500

    qualitative lsun

--- figure/qualitative_lsun.py
+++ figure/qualitative_lsun.py
@@ -31,22 +31,10 @@ model_files = [glob.glob(f"{f}/*.model")[0] for f in model_files]
 model_files.sort()
 func = get_semantic_extractor("linear")
 torch.manual_seed(65537)
-latents = torch.randn(len(model_files), 512).to(device)
+latents = torch.randn(len(model_files) * 4, 512).to(device)
 
-# get result from all models
-res = []
-for ind, model_file in enumerate(model_files):
-    task = utils.listkey_convert(model_file, ["bedroom", "church"])
-    model_name = utils.listkey_convert(
-        model_file, ["stylegan2", "stylegan", "proggan"])
-    model_path = f"checkpoint/{task}_lsun_256x256_{model_name}.pth"
-    print(f"=> load {model_name} from {model_path}")
-    generator = model.load_model_from_pth_file(
-        model_name,
-        model_path)
-    generator.to(device).eval()
-    
-    latent = latents[ind:ind+1]
+def get_output(generator, model_file, external_model, latent,
+    flag=2):
     with torch.no_grad():
         image, stage = generator.get_stage(latent)
         image = image.clamp(-1, 1)
@@ -65,12 +53,50 @@ for ind, model_file in enumerate(model_files):
     size = label.shape[2:]
     image = F.interpolate(image, size=size, mode="bilinear")
     image = (utils.torch2numpy(image[0]) + 1) * 127.5
-    res.append(image.transpose(1, 2, 0))
-    for i, seg in enumerate(multi_segs[:2]):
+    res = [image.transpose(1, 2, 0)]
+    for i, seg in enumerate(multi_segs[:flag]):
         label_viz = colorizer(label[0, i])
         pred_group = segmenter.convert_multi_label(
             seg, cg_label, i)
         pred_group_viz = colorizer(pred_group)
         res.extend([pred_group_viz, label_viz])
-res = torch.from_numpy(np.stack(res)).permute(0, 3, 1, 2).float() / 255.
-vutils.save_image(res, f"test.png", nrow=5)
+    return res
+
+
+def process(res):
+    res = torch.from_numpy(np.stack(res))
+    return res.permute(0, 3, 1, 2).float() / 255.
+
+
+# get result from all models
+paper_res = []
+appendix_res = []
+count = 0
+for ind, model_file in enumerate(model_files):
+    task = utils.listkey_convert(model_file, ["bedroom", "church"])
+    model_name = utils.listkey_convert(
+        model_file, ["stylegan2", "stylegan", "proggan"])
+    model_path = f"checkpoint/{task}_lsun_256x256_{model_name}.pth"
+    print(f"=> load {model_name} from {model_path}")
+    generator = model.load_model_from_pth_file(
+        model_name,
+        model_path)
+    generator.to(device).eval()
+
+    latent = latents[count:count+1]
+    paper_res.extend(get_output(
+        generator, model_file, external_model, latent,
+        flag=1))
+    count += 1
+    latent = latents[count:count+1]
+    appendix_res.extend(get_output(
+        generator, model_file, external_model, latent,
+        flag=3))
+    count += 1
+
+vutils.save_image(
+    process(paper_res),
+    f"qualitative_lsun_paper.png", nrow=6)
+vutils.save_image(
+    process(appendix_res),
+    f"qualitative_lsun_appendix.png", nrow=7)
