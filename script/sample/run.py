6137a8ed0dee4177ff10f8f9d17a76851f6b3581
AtlantixJJ <xujj15@mails.tsinghua.edu.cn>
2020-05-27 19:37:51 +0800

    git sync

--- script/sample/run.py
+++ script/sample/run.py
@@ -58,15 +58,6 @@ def command_sample_real(gpus):
             count += 1
 gpus = [0]
 
-def baseline_sample_real(gpus):
-    count = 0
-    basecmd = "python script/sample/baseline.py --outdir results/baseline_real --n-iter 1600 --n-total 8 --image ../datasets/CelebAMask-HQ/CelebA-HQ-img/%d.jpg --label ../datasets/CelebAMask-HQ/CelebAMask-HQ-mask-15/%d.png --gpu %d"
-    for i in range(8):
-        idx = count % len(gpus)
-        yield idx, basecmd % (i, i, gpus[idx])
-        count += 1
-gpus = [0]
-
 def command_pggan_fewshot_real(gpus):
     count = 0
     basecmd = "python script/sample/msreal_pggan.py --outdir results/sample_pggan_%d --n-iter 1000 --n-total 8 --image ../data/CelebAMask-HQ/CelebA-HQ-img/%d.jpg --label ../data/CelebAMask-HQ/CelebAMask-HQ-mask-15/%d.png --model results/svm_t%d_stylegan_linear_extractor_layer3,4,5,6,7.model --G checkpoint/face_celebahq_1024x1024_proggan.pth --resolution 512 --gpu %d"
@@ -78,9 +69,17 @@ def command_pggan_fewshot_real(gpus):
             count += 1
 gpus = [0, 1, 2, 3]
 
+def baseline_sample_real(gpus):
+    count = 0
+    basecmd = "python script/sample/baseline.py --outdir results/baseline_real --n-iter 1600 --n-total 8 --image ../datasets/CelebAMask-HQ/CelebA-HQ-img/%d.jpg --label ../datasets/CelebAMask-HQ/CelebAMask-HQ-mask-15/%d.png --gpu %d"
+    for i in range(8):
+        idx = count % len(gpus)
+        yield idx, basecmd % (i, i, gpus[idx])
+        count += 1
+gpus = [0]
 
 slots = [[] for _ in gpus]
-for i, c in command_pggan_fewshot_real(gpus):
+for i, c in baseline_sample_real(gpus):
     slots[i].append(c)
 
 for slot in slots:
