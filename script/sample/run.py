0228bd49f023ea8c0a8a9608dd05e92b68e37839
AtlantixJJ <xujj15@mails.tsinghua.edu.cn>
2020-05-31 18:58:16 +0800

    git sync

--- script/sample/run.py
+++ script/sample/run.py
@@ -110,15 +110,17 @@ elif sys.argv[1] == "1": # sample fewshot uper
         count = 0
         model_path = "bedroom_lsun_256x256_stylegan.pth"
         resolution = 256
+        l = ""
         if "church" in name:
             model_path = "church_lsun_256x256_stylegan2.pth"
         elif "cat" in name:
             model_path = "cat_lsun_256x256_stylegan2.pth"
         elif "car" in name:
             model_path = "car_lsun_512x384_stylegan2.pth"
+            l = ",7"
             resolution = 512
 
-        basecmd = f"python script/sample/msreal.py --outdir results/{name}_fewshot_real_%d --n-iter 3000 --n-total 8 --image {ds}/image%d.png --label {ds}/sv_label%d.npy --model results/fewshot_svm/svm_t%d_{name}_layer2,3,4,5,6_linear_extractor.model --G checkpoint/{model_path} --resolution {resolution} --gpu %d --method LL"
+        basecmd = f"python script/sample/msreal.py --outdir results/{name}_fewshot_real_%d --n-iter 3000 --n-total 8 --image {ds}/image%d.png --label {ds}/sv_label%d.npy --model results/fewshot_svm/svm_t%d_{name}_layer2,3,4,5,6{l}_linear_extractor.model --G checkpoint/{model_path} --resolution {resolution} --gpu %d --method LL"
 
         for t in [8]:
             for i in range(10):
