6edd64b5f5de31a51c55b90bf92cda5ca41d5081
AtlantixJJ <xujj15@mails.tsinghua.edu.cn>
2020-06-03 09:59:25 +0800

    git sync

--- figure/run_lsun_agreement.py
+++ figure/run_lsun_agreement.py
@@ -1,6 +1,6 @@
 import os
 cmds = [
-    f"python figure/lsun_agreement.py --dir record/lsun --model stylegan",
+    f"python figure/lsun_agreement.py --dir record/lsun --model stylegan_",
     f"python figure/lsun_agreement.py --dir record/lsun --model proggan",
     f"python figure/lsun_agreement.py --dir record/lsun --model stylegan2",]
 for cmd in cmds:
